30cdf785ef7fc1e5c4888de131fd224aac0d7b07
Kevin Weel <1828180+QandnotU@users.noreply.github.com>
2026-02-23 21:56:44 -0800

    release: 1.6.5-alpha.5

--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -107,7 +107,7 @@ export class PuraPlatformAccessory {
       .onGet(this.getOn.bind(this));
     if (this.useFanService) {
       this.service.getCharacteristic(this.platform.Characteristic.RotationSpeed)
-        .setProps({ validValues: [30, 50, 100] })
+        .setProps({ validValues: [0, 30, 50, 100] })
         .onSet(this.setRotationSpeed.bind(this))
         .onGet(this.getRotationSpeed.bind(this));
     }
